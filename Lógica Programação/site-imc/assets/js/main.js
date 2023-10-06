a0df750156951b8f9a4fbc77469b2c571787e276
henriquepenaforte <141761606+henriquepenaforte@users.noreply.github.com>
2023-10-06 10:35:15 -0300

    .

--- Lógica Programação/site-imc/assets/js/main.js
+++ Lógica Programação/site-imc/assets/js/main.js
@@ -7,31 +7,25 @@ function IMC () {
         } else if (altura > 2.5) {
             alert("Lembre-se de colocar a altura em metros!")
         } else {
-            let imc = Number(peso)/Number(altura) ** 2;
-            imc.toFixed(2);
+            let imc = (Number(peso)/Number(altura) ** 2);
+            imc = Number(imc.toFixed(1));
             res.innerHTML = `<p>IMC = ${imc}</p>`
-            res.style.backgroundColor = ""
-            switch (imc) {
-                case imc < 18.5: 
-                res.innerHTML += `<p> (Abaixo do Peso)</p>`
-                break
-                case imc < 25:
-                res.innerHTML += `<p> (Peso Normal)</p>`
-                break
-                case imc < 30:
-                res.innerHTML += `<p> (Sobrepeso)</p>`
-                break
-                case imc < 35:
-                res.innerHTML += `<p> (Obesidade grau 1)</p>`
-                break
-                case imc < 40:
-                res.innerHTML += `<p> (Obesidade grau 2)</p>`
-                break
-                case imc >= 40:
-                res.innerHTML += `<p> (Obesidade grau 3)</p>`
-                break
-                default:
-                alert('Valores inválidos')
+            res.style.backgroundColor = "rgba(255, 136, 0, 0.685)"
+                if (imc < 18.5) {
+                    res.innerHTML += `<p> (Abaixo do Peso)</p>`
+                } else if (imc < 25) {
+                    res.innerHTML += `<p> (Peso Normal)</p>`
+                    res.style.backgroundColor = "rgba(0, 255, 34, 0.596)";
+                } else if (imc < 30) {
+                    res.innerHTML += `<p> (Sobrepeso)</p>`
+                } else if (imc < 35) {
+                    res.innerHTML += `<p> (Obesidade grau 1)</p>`
+                } else if (imc < 40) {
+                    res.innerHTML += `<p> (Obesidade grau 2)</p>`
+                } else if (imc >= 40) {
+                    res.innerHTML += `<p> (Obesidade grau 3)</p>`
+                } else {
+                    alert('Valores inválidos')
+                }
             }
-        }
 }
