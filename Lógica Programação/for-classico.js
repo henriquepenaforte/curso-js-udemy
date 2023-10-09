dfaeaf48b015bbcb5d7ab9daaeda614be0c1d98d
henriquepenaforte <141761606+henriquepenaforte@users.noreply.github.com>
2023-10-09 11:24:54 -0300

    Create for-classico.js

---
+++ Lógica Programação/for-classico.js
@@ -0,0 +1,8 @@
+for (let c = 0; c <=10; c++) {
+    let par = c % 2 === 0 ? 'Par' : 'ímpar';
+    console.log(c, par);
+}
+
+const frutas = ['maçâ', 'pera', 'uva', 'banana', 'limao', 'laranja'];
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`índice ${i}, ${frutas[i]}`);
