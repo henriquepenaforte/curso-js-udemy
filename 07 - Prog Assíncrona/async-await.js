41d20cf72c3379be82bca047eecf56a8e125810b
henriquepenaforte <141761606+henriquepenaforte@users.noreply.github.com>
2024-01-11 17:08:53 -0300

    ajax

--- 07 - Prog Assíncrona/async-await.js
+++ 07 - Prog Assíncrona/async-await.js
@@ -17,7 +17,7 @@ function esperaAi (msg, tempo) {
     });
 }
 
-/* esperaAi('fase 1', aleatorio())
+esperaAi('fase 1', aleatorio())
     .then((valor) => {
         console.log(valor)
         return esperaAi('fase 2', aleatorio())
@@ -34,7 +34,7 @@ function esperaAi (msg, tempo) {
     })
     .catch(erro => {
         console.log(erro)
-    }); */
+    });
 
 async function executa () {
     try {
