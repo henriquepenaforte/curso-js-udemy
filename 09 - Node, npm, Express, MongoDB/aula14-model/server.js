83a353caac866dc0471f316ee7647b3a90e299be
henriquepenaforte <141761606+henriquepenaforte@users.noreply.github.com>
2024-02-06 12:23:42 -0300

    create first model w mongoose

---
+++ 09 - Node, npm, Express, MongoDB/aula14-model/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express'); //importando a biblioteca express;
+const app = express(); //carregando o express;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        app.emit('pronto')
+    })
+    .catch(e => console.log(e));
+
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+const { outroMiddleware } = require('./src/middlewares/middleware');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'public'))); 
+app.use(middlewareGlobal)
+app.use(outroMiddleware);
+
+
+// app.set('views', path.resolve(__dirname, 'src', 'views')); podendo passar o caminho absoluto por path, ou:
+app.set('views', './src/views') //passando o caminho relativo
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
+app.on('pronto', () => {
+    app.listen(3000, () => {
+        console.log('Acessando página http://localhost:3000')
+        console.log('Servidor executando na porta 3000')
+    });
+});
+
+
+
---
+++ 09 - Node, npm, Express, MongoDB/aula14-model/src/controllers/sobreController.js
@@ -0,0 +1,3 @@
+exports.paginaInicial = (req, res) => {
+    res.send('Sobre a página...')
+}
