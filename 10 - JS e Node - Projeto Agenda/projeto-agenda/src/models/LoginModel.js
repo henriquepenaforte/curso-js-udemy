f67ab9f40b6c9d1f8246584b229967ed1688672b
henriquepenaforte <141761606+henriquepenaforte@users.noreply.github.com>
2024-02-21 17:42:09 -0300

    added bcrypt passw
    & user already created

--- 10 - JS e Node - Projeto Agenda/projeto-agenda/src/models/LoginModel.js
+++ 10 - JS e Node - Projeto Agenda/projeto-agenda/src/models/LoginModel.js
@@ -2,6 +2,7 @@
 
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({ //define os campos que irão ser enviados e estarão presente na base de dados, informando também o tipo de dado que será guardado no campo informado: 
     email: { type: String, required: true },
@@ -20,11 +21,24 @@ class Login {
     async register() {
         this.valida();
         if (this.errors.length > 0) return;
+
+        await this.userExists();
+
+        if (this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
         try {
             this.user = await LoginModel.create(this.body);
         } catch(e) {
             console.log(e);
         }
+    }
+
+    async userExists() {
+        const user = await LoginModel.findOne({email: this.body.email});
+        if (user) this.errors.push('Usuário já existe');
     }
 
     valida() {
